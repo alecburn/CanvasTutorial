2a2f3db949935b2f87ba1299e63a4626a7511aa6
Alec <33267850+alecburn@users.noreply.github.com>
2019-06-08 12:56:53 -0400

    Worked with squares, lines, and arcs. Included colors.

--- canvas.js
+++ canvas.js
@@ -2,4 +2,24 @@ var canvas = document.querySelector('canvas');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-console.log(canvas);
+
+var c = canvas.getContext('2d');
+
+
+c.fillStyle = 'rgba(205,76,0, 0.5';
+c.fillRect(100,100,100,100);
+
+
+//line
+c.beginPath();
+c.moveTo(50,300);
+c.lineTo(300,100);
+c.lineTo(248,394);
+c.strokeStyle = "afa34a3";
+c.stroke();
+
+// arc/circle
+c.beginPath();
+c.arc(300,300,30,0, Math.PI * 2, false);
+c.strokeStyle = 'blue';
+c.stroke();

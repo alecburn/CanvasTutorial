2b969917a591ff864164bcd7de3d7e9e40d43686
alecburn <Alecburn@gmail.com>
2019-06-09 13:09:56 -0400

    Cleaned up files

--- canvas.js
+++ canvas.js
@@ -19,25 +19,6 @@ var colorArray = [
 var maxRadius = 40;
 var minRadius = 10;
 
-//rect
-//c.fillStyle = 'rgba(205,76,0, 0.5';
-//c.fillRect(100,100,100,100);
-//
-//
-// //line
-// c.beginPath();
-// c.moveTo(50,300);
-// c.lineTo(300,100);
-// c.lineTo(248,394);
-// c.strokeStyle = "afa34a3";
-// c.stroke();
-//
-// // arc/circle
-// c.beginPath();
-// c.arc(300,300,30,0, Math.PI * 2, false);
-// c.strokeStyle = 'blue';
-// c.stroke();
-
 
 window.addEventListener('mousemove', function (event) {
     mouse.x = event.x;
